d153de4071a8dd6c065722aa401244d8e76679c3
sashaz <6964653+OZahnitko@users.noreply.github.com>
2021-12-10 22:12:52 +1000

    Improved scripts?
    I hope that I was able to imrove the scripts!

--- terraform/scripts/setup_remote.sh
+++ terraform/scripts/setup_remote.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 # The starting directory for the script is realtive to package.json
 cd ./terraform
 
@@ -13,8 +12,10 @@ rm -rf terraform.pem | :
 terraform output -raw ssh_private_key > terraform.pem
 chmod 400 terraform.pem
 
+INSTANCE_PUBLIC_IP=$(terraform output -raw instance_public_ip)
+
 ssh \
-ec2-user@$(terraform output -raw instance_public_ip) \
+ec2-user@$INSTANCE_PUBLIC_IP \
 -i terraform.pem \
 -o StrictHostKeyChecking=no \
 << EOF
@@ -27,24 +28,26 @@ echo "I sleep"
 sleep 20
 echo "I still sleep"
 sleep 20
-echo "I still sleep"
+echo "I yet still sleep"
 sleep 20
 echo "Real s___!?"
 
 ssh \
 -fNtR \
-$(terraform output -raw instance_public_ip):3000:localhost:3000 \
-ec2-user@$(terraform output -raw instance_public_ip) \
+$INSTANCE_PUBLIC_IP:3000:localhost:3000 \
+ec2-user@$INSTANCE_PUBLIC_IP \
 -i terraform.pem \
 -o StrictHostKeyChecking=no
 
 # Post the public_ip address to Slack\
+[ $NODE_ENV = "dev" ] && SLACK_WEBHOOK_URL=$(grep SLACK_WEBHOOK_URL ./scripts/.env.local | cut -d '=' -f 2-) || SLACK_WEBHOOK_URL=$SLACK_WEBHOOK_URL
+
 curl \
 -X \
 POST \
 -H 'Content-type: application/json' \
---data '{"text":"'"http://$(terraform output -raw instance_public_ip):3000"'"}' \
-https://hooks.slack.com/services/"$(grep SLACK_WEB_HOOK_ADDRESS .env.local | cut -d '=' -f 2-)"
+--data '{"text":"'"http://$INSTANCE_PUBLIC_IP:3000"'"}' \
+$SLACK_WEBHOOK_URL
 
 # Remove the key
-rm -rf terraform.pem
+rm -rf terraform.pem || :
