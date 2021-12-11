e0b09586be28ce8005f2b3fe5121f2b6e16c9ce3
sashaz <6964653+OZahnitko@users.noreply.github.com>
2021-12-11 11:12:47 +1000

    static in a bucket

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,31 @@
+import axios from "axios";
+import { useState } from "react";
+
 const App = () => {
+  const [lineOne] = useState<string>("I like to poop and I cannot lie!");
+  const [lineTwo] = useState<string>("You other poopers can't deny!");
+  const [lineThree, setLineThree] = useState<string>("Poop come out my bum!");
+  const [lineFour, setLineFour] = useState<string>("");
+
   return (
     <div>
-      <h1>I like to poop and I cannot lie.</h1>
-      <h1>You other poopers can't deny.</h1>
-      <h1>Poop come out my bum.</h1>
+      <h1>{lineOne}</h1>
+      <h1>{lineTwo}</h1>
+      <h1>{lineThree}</h1>
+      <h1>{lineFour}</h1>
+      <button onClick={() => setLineThree(() => "SHIT get out of my ASS!!")}>
+        SHIT
+      </button>
+      <button
+        onClick={async () => {
+          const res = await axios.get(
+            "https://jsonplaceholder.typicode.com/todos/1"
+          );
+          setLineFour(() => res.data.title);
+        }}
+      >
+        GET MORE SHIT
+      </button>
     </div>
   );
 };
